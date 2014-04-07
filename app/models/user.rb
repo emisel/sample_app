57fd9691fcdad873e89d1ae9f37960b3ecc57de7
emisel <eselmeryd@gmail.com>
2014-04-07 20:19:13 +0200

    Adding email tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
