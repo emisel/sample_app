f9c3fdc8c284ceebbd5f4fc25c526343c4bcd71e
emisel <eselmeryd@gmail.com>
2014-06-02 20:38:20 +0200

    Adding password hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :email, uniqueness: { case_sensitive: false }, presence: true, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
