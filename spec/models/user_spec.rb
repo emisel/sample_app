852ced12497b89a4a950be39a4c26b1cd7c96712
emisel <eselmeryd@gmail.com>
2014-06-02 19:17:26 +0200

    Adding password field and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",password:"foobar",password_confirmation)
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
 
   it { should be_valid }
 
